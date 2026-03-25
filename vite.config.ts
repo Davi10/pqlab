899827d8e250b931dc920036545ba357ca744d68
DaviSantiago <santiago.davi10@gmail.com>
2026-03-25 14:49:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  // Troque '/' pelo nome do seu repositório no GitHub entre barras
+  base: '/pqlab/', 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
